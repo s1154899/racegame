0a983f28b4c3e3084f4acca67b8b23a52d73d616
s1154899 <s1154899@student.windesheim.nl>
2021-09-14 14:25:13 +0200

    level 2 -3

--- model/IParticipant.cs
+++ model/IParticipant.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace race
 {
-    class Participant
+    interface IParticipant
     {
     }
 }
--- model/SectionData.cs
+++ model/SectionData.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace race
 {
-    class Equipment
+    class SectionData
     {
     }
 }
